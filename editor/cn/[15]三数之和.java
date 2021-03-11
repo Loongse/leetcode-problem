c1d75b6c80d09076730aeb883c726c647a83df30
loongse <18707950895@163.com>
2021-03-11 16:22:54 +0800

    [15]三数之和
    先排序，然后i处如果num[i]>0说明无后续元素直接返回，（注意要在每一个添加三元组后省去重复数字）

---
+++ editor/cn/[15]三数之和.java
@@ -0,0 +1,74 @@
+//给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有和为 0 且不重
+//复的三元组。 
+//
+// 注意：答案中不可以包含重复的三元组。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [-1,0,1,2,-1,-4]
+//输出：[[-1,-1,2],[-1,0,1]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = []
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [0]
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= nums.length <= 3000 
+// -105 <= nums[i] <= 105 
+// 
+// Related Topics 数组 双指针 
+// 👍 3070 👎 0
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        int len = nums.length;
+        if (nums == null || len < 3) return res;
+        //对数组进行排序，如果num[i] > 0则后续没有满足情况的三元组
+        //重复元素跳过，避免出现重复解
+        Arrays.sort(nums);
+        for (int i = 0; i < len; i++) {
+            if (nums[i] > 0) break;//后续无满足解
+            if (i > 0 && nums[i] == nums[i - 1]) continue;//去重
+            int L = i + 1;
+            int R = len - 1;
+            while (L < R) {
+                int sum = nums[i] + nums[L] + nums[R];
+                if (sum == 0) {
+                    res.add(Arrays.asList(nums[i], nums[L], nums[R]));
+                    while (L < R && nums[L] == nums[L + 1]) L++;
+                    while (L < R && nums[R] == nums[R - 1]) R--;
+                    L++;
+                    R--;
+                } else if (sum < 0) L++;
+                else if (sum > 0) R--;
+            }
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
